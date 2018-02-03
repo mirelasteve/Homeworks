417ca601d27a78985b930a7007884cc32e03c127
mirelasteve <mirelasteve@abv.bg>
2018-02-03 10:22:51 +0200

    Create CrookedDigits.js

---
+++ CrookedDigits.js
@@ -0,0 +1,21 @@
+let n = gets();
+if (n.includes('-')) {
+    n = n.slice(1, n.length);
+}
+if (n.includes('.')) {
+    let ind = n.indexOf('.');
+    let m = n.slice(0, ind);
+    n = m + n.slice(ind + 1, n.length);
+};
+
+const crooked = (n) => {
+
+    if (n >= 1 && n <= 9) {
+        print(n);
+        return;
+    } else {
+        n = n.toString().split('').map(Number).reduce((x, y) => x + y);
+        crooked(n);
+    }
+}
+crooked(n);
